8d5035c14b30ba131b9ba0c2205988ab6efc066a
aa.bujko <aa.bujko@gmail.com>
2018-02-05 13:17:50 +0100

    changed config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'mypanelbase');
 define('DB_USER', 'mypanelbase');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Luci0010');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
